e0aee52b0f6acb042e592a524f954225e1c575e8
TWheatKing <andrew.wheatcroft2@gmail.com>
2023-03-04 12:53:47 +0000

    Initial commit

---
+++ src/main/java/thewheatking/themagicalmod/TheMagicalMod.java
@@ -0,0 +1,16 @@
+package thewheatking.themagicalmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheMagicalMod implements ModInitializer {
+		public static final String MOD_ID = "themagicalmod";
+		public static final Logger LOGGER = LoggerFactory.getLogger("themagicalmod");
+
+		@Override
+		public void onInitialize() {
+
+		}
+	}
---
+++ src/main/java/thewheatking/themagicalmod/TheMagicalModClient.java
@@ -0,0 +1,11 @@
+package thewheatking.themagicalmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheMagicalModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
