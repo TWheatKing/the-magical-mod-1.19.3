4ed2fbe51003930530dedc37e7a1434365522121
TWheatKing <andrew.wheatcroft2@gmail.com>
2023-03-07 12:55:42 +0000

    Changed the new trees look in ConfiguredFeatures

--- src/main/java/thewheatking/themagicalmod/world/ModConfiguredFeatures.java
+++ src/main/java/thewheatking/themagicalmod/world/ModConfiguredFeatures.java
@@ -12,7 +12,7 @@ import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
-import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
+import net.minecraft.world.gen.trunk.*;
 import thewheatking.themagicalmod.TheMagicalMod;
 import thewheatking.themagicalmod.block.ModBlocks;
 
@@ -24,19 +24,19 @@ public class ModConfiguredFeatures {
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context) {
         register(context, MAGICAL_WOOD_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
                 BlockStateProvider.of(ModBlocks.MAGICAL_WOOD_LOG),
-                new StraightTrunkPlacer(5, 6, 3),
+                new LargeOakTrunkPlacer(9,5,4),
                 BlockStateProvider.of(ModBlocks.MAGICAL_WOOD_LEAVES),
-                new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4),
+                new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 7),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
         register(context, ANCIENT_MAGICAL_WOOD_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
                 BlockStateProvider.of(ModBlocks.ANCIENT_MAGICAL_WOOD_LOG),
-                new StraightTrunkPlacer(5, 6, 3),
+                new ForkingTrunkPlacer(5, 6, 7),
                 BlockStateProvider.of(ModBlocks.ANCIENT_MAGICAL_WOOD_LEAVES),
                 new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
         register(context, DARK_MAGICAL_WOOD_KEY, Feature.TREE, new TreeFeatureConfig.Builder(
                 BlockStateProvider.of(ModBlocks.DARK_MAGICAL_WOOD_LOG),
-                new StraightTrunkPlacer(5, 6, 3),
+                new DarkOakTrunkPlacer(5, 6, 3),
                 BlockStateProvider.of(ModBlocks.DARK_MAGICAL_WOOD_LEAVES),
                 new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
